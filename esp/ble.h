f0c98874c1b1bc8e0d3d9e2031fafb9e312dc1db
picass02005 <clementduran0@gmail.com>
2025-12-20 23:24:48 +0100

    Reformated ESP code

--- esp/ble.h
+++ esp/ble.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2025 picasso2005 <clementduran0@gmail.com>
- * 
+ *
  * This software is released under the MIT License.
  * https://opensource.org/licenses/MIT
  */
--- esp/pins.h
+++ esp/pins.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2025 picasso2005 <clementduran0@gmail.com>
- * 
+ *
  * This software is released under the MIT License.
  * https://opensource.org/licenses/MIT
  */
@@ -10,7 +10,7 @@
 
 #define BUTTON 5      // Button is normally closed
 #define ONBOARD_LED 2 // Onboard LED
-#define LED_STRIP 15   // LED strip pin
+#define LED_STRIP 15  // LED strip pin
 #define LED_NB 8      // Number of LED
 
 // #define DEBUG  // Allow serial port prints
