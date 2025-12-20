93d601071469da9c01252780ed37cb1cf6e592a3
picass02005 <clementduran0@gmail.com>
2025-12-20 23:15:21 +0100

    Changed pin layout for prototype V3

--- esp/pins.h
+++ esp/pins.h
@@ -8,9 +8,9 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define BUTTON 15     // Button is normally closed
+#define BUTTON 5      // Button is normally closed
 #define ONBOARD_LED 2 // Onboard LED
-#define LED_STRIP 5   // LED strip pin
+#define LED_STRIP 15   // LED strip pin
 #define LED_NB 8      // Number of LED
 
 // #define DEBUG  // Allow serial port prints
